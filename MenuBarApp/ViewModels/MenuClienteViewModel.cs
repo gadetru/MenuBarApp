e86c78c2b958033c620bc05c504366851b4c366a
Gabriel Delgado <gadetru@gmail.com>
2026-04-07 21:40:42 +0200

    Se agrega el proyecto

---
+++ MenuBarApp/ViewModels/MenuClienteViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using MenuBarApp.Models;
+
+namespace MenuBarApp.ViewModels
+{
+    internal class MenuClienteViewModel
+    {
+        public ObservableCollection<Producto> Productos { get; set; }
+
+        public MenuClienteViewModel()
+        {
+            // prueba manual de correcto funcionamiento
+            Productos = new ObservableCollection<Producto>();
+            Productos.Add(new Producto { Nombre = "Hamburguesa", Precio = 5.50m });
+            Productos.Add(new Producto { Nombre = "Cerveza", Precio = 2.00m });
+        }
+
+    }
+}
---
+++ MenuBarApp/Views/MenuClienteView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MenuBarApp.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuBarApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MenuClienteView : Window
+    {
+        public MenuClienteView()
+        {
+            InitializeComponent();
+            DataContext = new MenuClienteViewModel();
+        }
+    }
+}
